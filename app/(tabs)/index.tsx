c0af915697627ea4b0ac43eb7136b18f0c35d105
adrian <adrian.chirinos@etec.sp.gov.br>
2024-10-24 17:57:43 -0300

    alterações de Menu

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,6 +4,7 @@ import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import { Collapsible } from "@/components/Collapsible";
 
 export default function HomeScreen() {
   return (
@@ -11,8 +12,8 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require("@/assets/images/boina.png")}
-          style={styles.reactLogo}
+          source={require("@/assets/images/banner.png")}
+        /*  style={styles.reactLogo}*/
         />
       }
     >
@@ -26,12 +27,16 @@ export default function HomeScreen() {
         <ThemedText type="subtitle"> Início do Projeto </ThemedText>
         <ThemedText>
           {" "}
-          O projeto começou com o objetivo de criar uma historia leve e de
-          grande entretenimento.{" "}
+          O projeto começou com o objetivo de criar uma história nova, um início do comércio de doces especializados com profissionalismo.{" "}
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+        <ThemedText type="subtitle">Atualizações do Projeto</ThemedText>
+       <Collapsible title="Início"> 
+       <ThemedText  >Após diversos estudos sobre esse mercado, foi tomada a decisão de iniciar.
+         </ThemedText>
+       </Collapsible> 
+      
         <ThemedText>
           Tap the Explore tab to learn more about what's included in this
           starter app.
@@ -62,11 +67,13 @@ const styles = StyleSheet.create({
     gap: 8,
     marginBottom: 8,
   },
+  /*
   reactLogo: {
     height: 250,
     width: 290,
     bottom: 0,
     left: 0,
     position: "absolute",
-  },
+  
+  },*/
 });
